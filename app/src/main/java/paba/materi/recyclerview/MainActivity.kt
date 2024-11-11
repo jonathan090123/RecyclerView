a48ae47c9facacedb4d42e4559fe9a420cb8ad23
Jonathan Alvin <hantububu5785@gmail.com>
2024-11-11 21:29:58 +0700

    sampe no 32

--- app/src/main/java/paba/materi/recyclerview/MainActivity.kt
+++ app/src/main/java/paba/materi/recyclerview/MainActivity.kt
@@ -1,30 +1,54 @@
 package paba.materi.recyclerview
 
+import android.annotation.SuppressLint
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var _nama : Array<String>
-    private lateinit var _karakter : Array<String>
-    private lateinit var _deskripsi : Array<String>
-    private lateinit var _gambar : Array<String>
-    private lateinit var _rvWayang : RecyclerView
+    private lateinit var _nama: Array<String>
+    private lateinit var _karakter: Array<String>
+    private lateinit var _deskripsi: Array<String>
+    private lateinit var _gambar: Array<String>
+    private lateinit var _rvWayang: RecyclerView
 
     private var arWayang = arrayListOf<wayang>()
 
+    @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        _rvWayang = findViewById(R.id.rvWayang)
+
+        SiapkanData()
+        TambahData()
+        TampilkanData()
+    }
+
+    private fun SiapkanData() {
+        _nama = resources.getStringArray(R.array.namaWayang)
+        _deskripsi = resources.getStringArray(R.array.deskripsiWayang)
+        _karakter = resources.getStringArray(R.array.karakterUtamaWayang)
+        _gambar = resources.getStringArray(R.array.gambarWayang)
+    }
+
+    private fun TambahData() {
+        for (position in _nama.indices) {
+            val data = wayang(
+                _gambar[position],
+                _nama[position],
+                _karakter[position],
+                _deskripsi[position]
+            )
+            arWayang.add(data)
         }
+    }
+
+    private fun TampilkanData() {
+        _rvWayang.layoutManager = LinearLayoutManager(this)
+        _rvWayang.adapter = adapterRecView(arWayang)
     }
 }
--- app/src/main/java/paba/materi/recyclerview/adapterRecView.kt
+++ app/src/main/java/paba/materi/recyclerview/adapterRecView.kt
@@ -1,17 +1,21 @@
 package paba.materi.recyclerview
 
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
+import com.squareup.picasso.Picasso
 
-class adapterRecView (private val listwayang: ArrayList<wayang>) : RecyclerView.Adapter<adapterRecView.ListViewHolder> () {
-    inner class ListViewHolder(itemView : View) : RecyclerView.ViewHolder(itemView) {
-        var _namaWayang = itemView.findViewById<TextView>(R.id.namaWayangTV)
-        var _karakterWayang = itemView.findViewById<TextView>(R.id.karakterWayangTV)
-        var _deskripsiWayang = itemView.findViewById<TextView>(R.id.deskripsiWayangTV)
-        var _gambarWayang = itemView.findViewById<TextView>(R.id.gambarWayangIV)
+class adapterRecView(private val listWayang: ArrayList<wayang>) : RecyclerView.Adapter<adapterRecView.ListViewHolder>() {
+
+    inner class ListViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        var _namaWayang = itemView.findViewById<TextView>(R.id.namaWayang)
+        var _karakterWayang = itemView.findViewById<TextView>(R.id.karakterWayang)
+        var _deskripsiWayang = itemView.findViewById<TextView>(R.id.deskripsiWayang)
+        var _gambarWayang = itemView.findViewById<ImageView>(R.id.gambarWayang)
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ListViewHolder {
@@ -20,14 +24,18 @@ class adapterRecView (private val listwayang: ArrayList<wayang>) : RecyclerView.
     }
 
     override fun getItemCount(): Int {
-        return listwayang.size
+        return listWayang.size
     }
 
     override fun onBindViewHolder(holder: ListViewHolder, position: Int) {
-        var wayang = listwayang[position]
+        val wayang = listWayang[position]
 
         holder._namaWayang.setText(wayang.nama)
-        holder._deskripsiWayang.setText(wayang.deskripsi)
         holder._karakterWayang.setText(wayang.karakter)
+        holder._deskripsiWayang.setText(wayang.deskripsi)
+        Log.d("TEST", wayang.foto)
+        Picasso.get()
+            .load(wayang.foto)
+            .into(holder._gambarWayang)
     }
 }
